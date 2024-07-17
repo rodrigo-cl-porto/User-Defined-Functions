08f7f811000cf99a6c26e9738c6fe84b7cd82c66
Rodrigo Celso de Lima Porto <79947127+rodrigo-cl-porto@users.noreply.github.com>
2024-07-17 08:08:20 -0300

    Create StringEndsWith.vb

---
+++ VBA/StringEndsWith.vb
@@ -0,0 +1,21 @@
+Public Function StringEndsWith(String1 As String, String2 As String, Optional CaseSensitive As Boolean = False) As Boolean
+    
+    If CaseSensitive Then
+    
+        If InStr(String1, String2) = Len(String1) - Len(String2) + 1 Then
+            StringEndsWith = True
+        Else
+            StringEndsWith = False
+        End If
+        
+    Else
+    
+        If InStr(1, String1, String2, vbTextCompare) = Len(String1) - Len(String2) + 1 Then
+            StringEndsWith = True
+        Else
+            StringEndsWith = False
+        End If
+        
+    End If
+
+End Function
