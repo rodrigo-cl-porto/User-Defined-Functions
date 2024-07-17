659ff61a61e76f299169a29915ac1af9b3f34ac7
Rodrigo Celso de Lima Porto <79947127+rodrigo-cl-porto@users.noreply.github.com>
2024-07-17 08:13:19 -0300

    Create SubStringIsInArray.vb

---
+++ VBA/SubStringIsInArray.vb
@@ -0,0 +1,16 @@
+Public Function SubStringIsInArray(SubString As String, SourceArray As Variant, Optional CaseSensitive As Boolean = False) As Boolean
+
+    Dim i As Long
+    
+    For i = 0 To UBound(SourceArray)
+        
+        If VarType(SourceArray(i)) = vbString Then
+            If StringContains(SourceArray(i), SubString, CaseSensitive) Then
+                SubStringIsInArray = True
+                Exit Function
+            End If
+        End If
+    
+    Next i
+
+End Function
