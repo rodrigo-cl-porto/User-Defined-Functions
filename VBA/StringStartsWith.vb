9541be996869c192014e8db8787c20d135619180
Rodrigo Celso de Lima Porto <79947127+rodrigo-cl-porto@users.noreply.github.com>
2024-07-17 08:07:44 -0300

    Create StringStartsWith.vb

---
+++ VBA/StringStartsWith.vb
@@ -0,0 +1,21 @@
+Public Function StringStartsWith(String1 As String, String2 As String, Optional CaseSensitive As Boolean = False) As Boolean
+
+    If CaseSensitive Then
+    
+        If InStr(String1, String2) = 1 Then
+            StringStartsWith = True
+        Else
+            StringStartsWith = False
+        End If
+        
+    Else
+    
+        If InStr(1, String1, String2, vbTextCompare) = 1 Then
+            StringStartsWith = True
+        Else
+            StringStartsWith = False
+        End If
+        
+    End If
+
+End Function
