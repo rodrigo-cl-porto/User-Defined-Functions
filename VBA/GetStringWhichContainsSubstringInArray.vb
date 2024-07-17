a005743fa9d70547ab68c43bb0b5d594cebc68e8
Rodrigo Celso de Lima Porto <79947127+rodrigo-cl-porto@users.noreply.github.com>
2024-07-17 08:13:51 -0300

    Create GetStringWhichContainsSubstringInArray.vb

---
+++ VBA/GetStringWhichContainsSubstringInArray.vb
@@ -0,0 +1,14 @@
+Public Function GetStringWhichContainsSubstringInArray(SubString As String, SourceArray As Variant, Optional CaseSensitive As Boolean = False) As String
+
+    Dim i As Long
+    
+    For i = 0 To UBound(SourceArray)
+        If VarType(SourceArray(i)) = vbString Then
+            If StringContains(SourceString, SubString, CaseSensitive) Then
+                GetStringWhichContainsSubstringInArray = SourceArray(i)
+                Exit Function
+            End If
+        End If
+    Next i
+
+End Function
