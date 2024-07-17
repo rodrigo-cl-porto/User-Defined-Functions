cf0208944e971836deb99ccc58c4ba8a483def4a
Rodrigo Celso de Lima Porto <79947127+rodrigo-cl-porto@users.noreply.github.com>
2024-07-17 08:11:09 -0300

    Create WorksheetHasListObject.vb

---
+++ VBA/WorksheetHasListObject.vb
@@ -0,0 +1,9 @@
+Public Function WorksheetHasListObject(ws As Worksheet) As Boolean
+
+    If ws.ListObjects.Count >= 1 Then
+        WorksheetHasListObject = True
+    Else
+        WorksheetHasListObject = False
+    End If
+    
+End Function
