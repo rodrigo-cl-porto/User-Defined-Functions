ff7db6ca0843e6b0fce3e59cd91cba76d15ed70c
Rodrigo Celso de Lima Porto <79947127+rodrigo-cl-porto@users.noreply.github.com>
2024-07-17 08:09:56 -0300

    Create RangeIsHidden.vb

---
+++ VBA/RangeIsHidden.vb
@@ -0,0 +1,25 @@
+Public Function RangeIsHidden(rng As Range) As Boolean
+
+    Dim ErrNumber As Integer
+    Dim ErrText   As String
+    
+    If rng Is Nothing Then
+        RangeIsHidden = True
+        Exit Function
+    End If
+    
+    On Error Resume Next
+    rng.SpecialCells (xlCellTypeVisible)
+    ErrNumber = Err.Number
+    ErrText = Err.Description
+    On Error GoTo 0
+    
+    If ErrNumber = 0 Then
+        RangeIsHidden = False
+    ElseIf ErrText = "No cells were found." Then
+        RangeIsHidden = True
+    Else
+        MsgBox "The following error occured: " & ErrNumber & vbLf & vbLf & ErrText, vbCritical + vbOKOnly, "Message Error"
+    End If
+
+End Function
