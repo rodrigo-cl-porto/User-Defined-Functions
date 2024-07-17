79e9443fe98b060ad1f44ba07e224f1bc8d758bc
Rodrigo Celso de Lima Porto <79947127+rodrigo-cl-porto@users.noreply.github.com>
2024-07-17 08:08:52 -0300

    Create StringContains.vb

---
+++ VBA/StringContains.vb
@@ -0,0 +1,21 @@
+Public Function StringContains(String1 As String, String2 As String, Optional CaseSensitive As Boolean = False) As Boolean
+    
+    If CaseSensitive Then
+    
+        If InStr(String1, String2) > 0 Then
+            StringContains = True
+        Else
+            StringContains = False
+        End If
+        
+    Else
+    
+        If InStr(1, String1, String2, vbTextCompare) > 0 Then
+            StringContains = True
+        Else
+            StringContains = False
+        End If
+        
+    End If
+
+End Function
