79ca16c8d252d987701235bd8fb34b081487f1d3
Rodrigo Celso de Lima Porto <79947127+rodrigo-cl-porto@users.noreply.github.com>
2024-11-07 17:08:23 -0300

    Create GetLettersOnly.vb

---
+++ VBA/GetLettersOnly.vb
@@ -0,0 +1,21 @@
+Function GetLettersOnly(Text As String) As String
+
+    Dim i       As Long
+    Dim Letters As String
+    Dim Chr     As String
+
+    Letters = vbNullString
+
+    For i = 1 To Len(Text)
+    
+        Chr = LCase(Mid(Text, i, 1))
+
+        If Asc(Chr) >= 97 And Asc(Chr) <= 122 Then
+            Letters = Letters + Chr
+        End If
+        
+    Next i
+    
+    GetLettersOnly = Letters
+    
+End Function
